7ca4bd5ccdb056dbc58b527b0270a06d0b109800
ArsalJan34 <arsaljaan532@gmail.com>
2025-11-07 21:13:06 +0500

    Changes made

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-document.querySelector('.form').addEventListener('.submit', async function (e) {
+document.querySelector('.contact-form').addEventListener('submit', async function (e) {
   e.preventDefault();
+
   const form = e.target;
   const data = new FormData(form);
-  const response = await fetch(form.action,
-    {
-    method: POST,
-    body : data
+
+  const response = await fetch(form.action, {
+    method: "POST",
+    body: data
   });
+
   const result = await response.json();
+
   if (result.success) {
-    alert(
-      'Thank you! Your order has been traced Successfully.'
-);
-form.reset();
-} else {
-  alert('Something Went Wrong, Pleae Try Again');
-}
+    alert('✅ Thank you! Your order has been traced successfully.');
+    form.reset();
+  } else {
+    alert('❌ Something went wrong, please try again.');
+  }
 });
